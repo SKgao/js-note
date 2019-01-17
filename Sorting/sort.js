b7ba2903a4fbdc3e160ab4dc8a332dd941e20b95
gaosikang <498039142@qq.com>
2019-01-17 14:16:11 +0800

    update Sorting

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -3,6 +3,13 @@
  */
 
 
+// 判断是否为数组
+var isArray = function (arr) {
+    return Object.prototype.toString.call(arr) === '[object Array]'
+}
+
+var testArr = [100, 37, 348, 838, 93, 6, 77, 3, 44, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 48]
+
 /*===================== 冒泡排序 ==========================*/
 var bubbleSort
 
@@ -40,8 +47,7 @@ bubbleSort = function (arr) {
     }
     return arr
 }
-
-export default bubbleSort
+console.log('bubbleSort______>', bubbleSort(testArr))
 
 /*===================== 选择排序 ==========================*/
 var selectionSort
@@ -49,11 +55,12 @@ var selectionSort
 // 最稳定的排序算法之一
 selectionSort = function (arr) {
     var len = arr.length
+    var minIdx = 0
     for (var i = 0; i < len - 1; i++) {
-        var minIdx = i
-        for (var j = i + 1; j < length; j++) {
-            if (arr[j] > arr[j + 1]) {
-                minIdx = j
+        minIdx = i
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j  // 找到最小数位置
             }
         }
         var temp = arr[i]
@@ -62,5 +69,198 @@ selectionSort = function (arr) {
     }
     return arr
 }
+console.log('selectionSort___>', selectionSort(testArr))
 
-export default selectionSort
+
+/*===================== 插入排序 ==========================*/
+var insertionSort // 类似于扑克牌发牌后的整理排序
+
+// 第一种
+// 通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入
+insertionSort = function (arr) {
+    if (isArray(arr)) {
+        for (var i = 0; i < arr.length; i++) {
+            var temp = arr[i]
+            var j = i - 1
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j]
+                j--
+            }
+            arr[j + 1] = temp
+        }
+        return arr
+    } else {
+        console.error('Parameter must be an array')
+    }
+}
+
+// 第二种
+// 查找插入位置时使用二分查找的方式
+insertionSort = function (arr) {
+    if (isArray(arr)) {
+        var len = arr.length
+        for (var i = 0; i < len; i++) {
+            var temp = arr[i]
+            var left = 0
+            var right = i - 1
+            while (left <= right) {
+                var middle = (left + right) / 2 | 0
+                if (temp < arr[middle]) {
+                    right = middle - 1
+                } else {
+                    left = middle + 1
+                }
+            }
+            for (var j = i - 1; j >= left; j--) {
+                arr[j + 1] = arr[j]
+            }
+            arr[left] = temp
+        }
+        return arr
+    } else {
+        console.error('Parameter must be an array')
+    }
+}
+console.log('insertionSort___>', insertionSort(testArr))
+
+
+/*===================== 希尔排序 ==========================*/
+var shellSort
+
+// 是简单插入排序的改进版；它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+shellSort = function (arr) {
+    var len = arr.length
+    var gap = 1
+    while (gap < len / 5) { // 自定义间隔段
+        gap = gap * 5 + 1
+    }
+    for (gap; gap > 0; gap = Math.floor(gap / 5)) {
+        for (var i = gap; i < len; i++) {
+            var temp = arr[i]
+            for (var j = i- gap; j >= 0 && arr[j] > temp; j -= gap) {
+                arr[j + gap] = arr[j]
+            }
+            arr[j + gap] = temp
+        }
+    }
+    return arr
+}
+console.log('shellSort_______>', shellSort(testArr))
+
+/*===================== 快速排序 ==========================*/
+var quickSort
+
+// 1.在数据集之中，选择一个元素作为"基准"（pivot）。
+// 2.所有小于"基准"的元素，都移到"基准"的左边；所有大于"基准"的元素，都移到"基准"的右边。
+// 3.对"基准"左边和右边的两个子集，不断重复第一步和第二步，直到所有子集只剩下一个元素为止。
+quickSort = function (arr) {
+    if (isArray(arr)) {
+        var len = arr.length
+        if (len <= 1) {
+            return arr
+        }
+        var pivotIndex = Math.floor(len / 2)
+        var pivot = arr.splice(pivotIndex, 1)[0]
+        var left = []
+        var right = []
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+                left.push(arr[i])
+            } else {
+                right.push(arr[i])
+            }
+        }
+        return quickSort(left).concat([pivot], quickSort(right))
+    } else {
+        console.error('Parameter must be an array')
+    }
+}
+
+
+/*===================== 归并排序 ==========================*/
+var mergeSort
+
+// 1.把长度为n的输入序列分成两个长度为n/2的子序列；
+// 2.对这两个子序列分别采用归并排序；
+// 3.将两个排序好的子序列合并成一个最终的排序序列
+mergeSort = function (arr) {
+    var len = arr.length
+    if (len <= 1) {
+        return arr
+    }
+    var middle = ~~(len / 2)
+    var left = arr.slice(0, middle)
+    var right = arr.slice(middle)
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+var merge = function (left, right) {
+    var result = []
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift())
+        } else {
+            result.push(right.shift())
+        }
+    }
+    return result.concat(left, right)
+}
+console.log('mergeSort_______>', mergeSort(testArr))
+
+/*===================== 计数排序 ==========================*/
+var countingSort
+
+// 1. 找出待排序的数组中最大和最小的元素；
+// 2. 统计数组中每个值为i的元素出现的次数，存入数组C的第i项；
+// 3. 对所有的计数累加（从C中的第一个元素开始，每一项和前一项相加）；
+// 4. 反向填充目标数组：将每个元素i放在新数组的第C(i)项，每放一个元素就将C(i)减去1。
+
+countingSort = function (arr) {
+    var len = arr.length
+    var B = []
+    var C = []
+    var min = arr[0]
+    var max = arr[0]
+    // 获取数组最大值、最小值，并找出数组每项的出现次数
+    for (var i = 0; i < len; i++) {
+        min = Math.min(min, arr[i])
+        max = Math.max(max, arr[i])
+        C[arr[i]] = C[arr[i]] ? C[arr[i]] + 1 : 1
+    }
+    // 将计数逐项累加
+    for (var j = min; j < max; j++) {
+        C[j + 1] = (C[j] || 0) + (C[j + 1] || 0)
+    }
+    // 反向填充数组
+    for (var k = len - 1; k >= 0; k--) {
+        B[C[arr[k]] - 1] = arr[k]
+        C[arr[k]]--
+    }
+    return B
+}
+console.log('countingSort____>', countingSort(testArr))
+console.log('quickSort_______>', quickSort(testArr))
+
+/*===================== 桶排序 ==========================*/
+var bucketSort
+
+
+/*===================== 堆排序 ==========================*/
+var heapSort
+
+
+/*===================== 基数排序 ==========================*/
+var radixSort
+
+export default {
+    bubbleSort,         // 冒泡排序
+    selectionSort,      // 选择排序
+    insertionSort,      // 插入排序
+    shellSort,          // 希尔排序
+    quickSort,          // 快速排序
+    mergeSort,          // 归并排序
+    countingSort,       // 计数排序
+    bucketSort,         // 桶排序
+    heapSort,           // 堆排序
+    radixSort           // 基数排序
+}
