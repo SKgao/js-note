1707cc0797463b43a6b8cd5cc171ef252e1233c4
gaosikang <498039142@qq.com>
2019-09-05 18:22:21 +0800

    update

--- Promise/index.js
+++ Promise/index.js
@@ -1,11 +1,71 @@
-// promise
-function Promise(cb) {
-    this.state = 'pending'
-    this.resolvedCallback = []
-    this.rejectedCallback = []
+/**
+ * Promise
+ */
+
+class Promise {
+    constructor(excutorCallback) {
+        this.status = 'pending';
+        this.value = undefined;
+        this.fulfilledArray = [];
+        this.rejectedArray = [];
+
+        let resovleFn = result => {
+            if (this.status !== 'pending') return;
+            let timer = setTimeout(() => {
+                this.status = 'fulfilled';
+                this.value = result;
+                this.fulfilledArray.forEach(item => item(this.value));
+            }, 0);
+        }
+
+        let rejectedFn = reason => {
+            if (this.status !== 'pending') return;
+            let timer = setTimeout(() => {
+                this.status = 'rejected';
+                this.value = reason;
+                this.rejectedArray.forEach(item => item(this.value));
+            }, 0);
+        }
+
+        try {
+            excutorCallback(resovleFn, rejectedFn);
+        } catch (error) {
+            rejectedFn(error);
+        }
+    }
+
+
+    then(fulfilledCallback, rejectedCallback) {
+        typeof fulfilledCallback === 'function' ? fulfilledCallback = result => result : null;
+        typeof rejectedCallback === 'function' ? rejectedCallback = reason => {
+            throw new Error(reason instanceof Error ? reason.message : reason);
+        } : null;
+
+        return new Promise((resolve, reject) => {
+            this.fulfilledArray.push(() => {
+                try {
+                    let v = fulfilledCallback(this.value);
+                    v instanceof Promise ? v.then(resolve, reject) : resolve(v);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+
+            this.rejectedArray.push(() => {
+                try {
+                    let v = rejectedCallback(this.value);
+                    v instanceof Promise ? v.then(resolve, reject) : resolve(v);
+                } catch (error) {
+                    reject(error);
+                }
+            })
+        })
+    }
+
+    catch(rejectedCallback) {
+        return this.then(null, rejectedCallback);
+    }
 
 }
 
-Promise.prototype.then = function (onFulfilled, onRejected) {
-
-}
+module.exports = Promise;
